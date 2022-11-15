98758cec07d2727f1be1aadd987999fe2d2ed5a8
Matheus LCSantos <matheuslcsantosbh@gmail.com>
2022-11-15 11:31:03 -0300

    Primeiros passos com classes

---
+++ Program4.cs
@@ -0,0 +1,38 @@
+namespace Projeto3;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Triangulo x, y;
+        x = new Triangulo();
+        y = new Triangulo();
+
+        Console.WriteLine("Entre com as medidas do triângulo X: ");
+        x.A = double.Parse(Console.ReadLine());
+        x.B = double.Parse(Console.ReadLine());
+        x.C = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Entre com as medidas do triânguloY: ");
+        y.A = double.Parse(Console.ReadLine());
+        y.B = double.Parse(Console.ReadLine());
+        y.C = double.Parse(Console.ReadLine());
+
+        double p = (x.A + x.B + x.C) / 2;
+        double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+
+        p = (y.A + y.B + y.C) / 2;
+        double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+
+        Console.WriteLine("Área de X : " + areaX.ToString());
+        Console.WriteLine("Área de y : " + areaY.ToString());
+
+        if (areaX > areaY)
+        {
+            Console.WriteLine("Maior área é a do triângulo X");
+        }
+        else
+        {
+            Console.WriteLine("Maior área é a do triângulo Y");
+        }
+    }
+}
